1a85e2d4459fff5fa025f5c5ba8d0213d87c2d87
Dong <964708273@qq.com>
2019-12-14 16:27:16 +0800

    添加国际化（未成功）

--- mango-ui/src/main.js
+++ mango-ui/src/main.js
@@ -4,9 +4,11 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import api from './http'
+import il8n from './il8n'
 import global from '@/utils/global'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import 'font-awesome/css/font-awesome.min.css'
 
 Vue.config.productionTip = false
 
@@ -16,7 +18,9 @@ Vue.prototype.global = global//挂载全局配置
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  il8n,
   router,
-  components: { App },
+  components: {App},
   template: '<App/>'
+
 })
--- mango-ui/src/views/Home.vue
+++ mango-ui/src/views/Home.vue
@@ -1,10 +1,15 @@
 <template>
   <div class="page">
     <h2>Home Page</h2>
+    <li class="fa fa-home fa-lg"></li>
     <!--Axios 是一个基于 promise 的 HTTP 库，可以用在浏览器和 node.js 中。-->
     <el-button type="primary" @click="testAxios()">测试Axios调用</el-button>
     <el-button type="primary" @click="getUser()">获取用户信息</el-button>
     <el-button type="primary" @click="getMenu()">获取菜单信息</el-button>
+    <!--<h3 >{{$t('common.doc')}}</h3>-->
+    <h3>国际化</h3>
+    <el-button type="success" @click="changeLanguage('zh_cn')">简体中文</el-button>
+    <el-button type="success" @click="changeLanguage('en_us')">English</el-button>
   </div>
 </template>
 
@@ -12,25 +17,31 @@
   import axios from 'axios'
 
   export default {
-        name: "Home",
-        methods: {
-            testAxios() {
-                axios.get('http://localhost:8080').then(res => {
-                    alert(res.data)
-                })
-            },
-            getUser() {
-                axios.get('http://localhost:8080/user').then(res => {
-                    alert(JSON.stringify(res.data))
-                })
-            },
-            getMenu() {
-                axios.get('http://localhost:8080/menu').then(res => {
-                    alert(JSON.stringify(res.data))
-                })
-            }
-        }
+    name: "Home",
+    methods: {
+      changeLanguage(lang) {
+        debugger
+        lang === '' ? 'zh_cn' : lang
+        this.$i18n.locale = lang
+        // this.langVisible = false
+      },
+      testAxios() {
+        axios.get('http://localhost:8080').then(res => {
+          alert(res.data)
+        })
+      },
+      getUser() {
+        axios.get('http://localhost:8080/user').then(res => {
+          alert(JSON.stringify(res.data))
+        })
+      },
+      getMenu() {
+        axios.get('http://localhost:8080/menu').then(res => {
+          alert(JSON.stringify(res.data))
+        })
+      }
     }
+  }
 
 </script>
 

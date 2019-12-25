a02f1f09878c68e570166a33ecd219a290d83ad1
liu-dong <964708273@qq.com>
2019-12-25 15:47:55 +0800

    添加服务注册中心

--- mango-admin/src/main/java/com/louis/mango/admin/MangoAdminApplication.java
+++ mango-admin/src/main/java/com/louis/mango/admin/MangoAdminApplication.java
@@ -2,7 +2,9 @@ package com.louis.mango.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 //(scanBasePackages={"com.louis.mango"}) 不加也没关系，springboot会默认扫描启动类的当前包及子包
 @SpringBootApplication(scanBasePackages={"com.louis.mango.admin"})
 public class MangoAdminApplication {
